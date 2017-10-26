74cd9ecc053d4d8dce3bff4aa1568fd77ff2e2a3
ArcInk <2482494917@qq.com>
2017-10-26 14:57:37 +0800

    加入空的SigninController.java，配置还没有没有完成

---
+++ src/main/java/xyz/hitpy/seproject/SigninController.java
@@ -0,0 +1,12 @@
+package xyz.hitpy.seproject;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author skill
+ * Handle request about signin.
+ */
+@Controller
+public class SigninController {
+    
+}
