c80d00c33dac0ddf1569ff4360ade5d68c097979
KentonJiang <jiangzhenfei1997@hotmail.com>
2017-10-28 09:35:49 +0800

    login and logout finished

---
+++ src/main/java/xyz/hitpy/seproject/controller/Login.java
@@ -0,0 +1,95 @@
+package xyz.hitpy.seproject.controller;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import xyz.hitpy.seproject.mysqlcon.SqlCon;
+
+@Controller
+public class Login {
+		
+	@RequestMapping("/login")
+	public String loginPage(ModelMap model) {
+		model.addAttribute("error", "");
+		return "login";
+	}
+	
+	/**
+     * @RequestParam 
+     * @param username
+     * @param password
+     * @param model
+     * @return
+     */
+	@RequestMapping("/loginnow")
+	public String login(@RequestParam("username") String username, 
+			@RequestParam("password") String password, ModelMap model,
+			HttpServletResponse response,HttpServletRequest request) {
+		boolean vali = false;
+		int autoLoginTimeout = -1;
+		SqlCon c = new SqlCon();
+		String pass = null;
+		ResultSet res = c.executeQuery("SELECT * FROM USER WHERE USERNAME = " + "\"" + username + "\"");
+		try {
+			if (res.first()) {
+				pass = res.getString("password");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		if (password != null && username != null && pass.equals(password)) {
+			autoLoginTimeout = Integer.MAX_VALUE;
+			vali = true;
+		}
+		if (vali) {
+			model.addAttribute("username", username);
+			request.getSession().setAttribute("username", username);
+			request.getSession().setAttribute("password", password);
+			return "index";
+		}
+		if(autoLoginTimeout > 0){  
+		    //自动登录cookie  
+		    Cookie userNameCookie = new Cookie("loginUserName", username);  
+		    Cookie passwordCookie = new Cookie("loginPassword", password);  
+		    userNameCookie.setMaxAge(autoLoginTimeout);  
+		    userNameCookie.setPath("/");  
+		    passwordCookie.setMaxAge(autoLoginTimeout);  
+		    passwordCookie.setPath("/");  
+		    response.addCookie(userNameCookie);  
+		    response.addCookie(passwordCookie);  
+		}  
+		else
+			model.addAttribute("error", "用户名错误或密码不存在");
+			return "login";
+		
+	}
+	
+	@RequestMapping("/logout")  
+	public String logout(HttpServletRequest request, HttpServletResponse response, Model model){  
+	    String username = (String) request.getSession().getAttribute("username");  
+	    String password = (String) request.getSession().getAttribute("password");   
+	    //删除登录cookie  
+	    Cookie userNameCookie = new Cookie("loginUserName", username );  
+	    Cookie passwordCookie = new Cookie("loginPassword", password);  
+	    userNameCookie.setMaxAge(0);  
+	    userNameCookie.setPath("/");  
+	    passwordCookie.setMaxAge(0);  
+	    passwordCookie.setPath("/");  
+	    response.addCookie(userNameCookie);  
+	    response.addCookie(passwordCookie);    
+	    request.getSession().removeAttribute("username");  
+	    request.getSession().removeAttribute("password");  
+	    return "redirect:xxx";  
+	}  
+}
