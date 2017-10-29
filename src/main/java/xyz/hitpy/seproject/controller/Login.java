21de9c486935d0d80163be8ac6bdebd1ed3c949c
KentonJiang <jiangzhenfei1997@hotmail.com>
2017-10-29 21:24:50 +0800

    repair classnotfound

--- src/main/java/xyz/hitpy/seproject/controller/Login.java
+++ src/main/java/xyz/hitpy/seproject/controller/Login.java
@@ -11,13 +11,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import xyz.hitpy.seproject.mysqlcon.SqlCon;
 
 @Controller
 public class Login {
 		
-	@RequestMapping("/login")
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginPage(ModelMap model) {
 		model.addAttribute("error", "");
 		return "login";
