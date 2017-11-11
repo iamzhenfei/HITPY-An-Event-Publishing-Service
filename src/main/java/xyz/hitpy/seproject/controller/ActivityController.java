38669c12e449dcdaed7e971ab1ae209b8665a855
ArcInk <2482494917@qq.com>
2017-11-11 20:08:06 +0800

    添加了单次查询帖子全部信息。修改了存帖子的验证方式。

--- src/main/java/xyz/hitpy/seproject/controller/ActivityController.java
+++ src/main/java/xyz/hitpy/seproject/controller/ActivityController.java
@@ -2,6 +2,7 @@ package xyz.hitpy.seproject.controller;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,33 +16,26 @@ import xyz.hitpy.seproject.mysqlcon.SqlCon;
 
 @Controller
 public class ActivityController {
-    @RequestMapping("create_activity")
+    @RequestMapping("/create_activity")
     public String createActivityPage() { return "create_activity"; }
     
-    @RequestMapping("postnow")
+    @RequestMapping("/postnow")
     public String addActivity(@RequestParam("eventName") String eventName, @RequestParam("eventLocation") String eventLocation,
             @RequestParam("eventTime") String eventTime, @RequestParam("content") String content, ModelMap model,
             HttpServletResponse response,HttpServletRequest request)
     {
-        String username = (String) request.getSession().getAttribute("username");  
-        String password = (String) request.getSession().getAttribute("password");
+        String username = (String) request.getSession().getAttribute("username");
+        if (username == null || username.equals("")) { return "redirect:login"; }
         SqlCon c = new SqlCon();
-        String pass = null;
         int uid = 0;
-        ResultSet res = c.executeQuery("SELECT * FROM sedb.user WHERE USERNAME = " + "\"" + username + "\"");
+        ResultSet res = c.executeQuery("SELECT * FROM sedb.user WHERE USERNAME = " + "\"" + username + "\" limit 0, 1");
         try {
             if (res != null && res.first()) {
-                pass = res.getString("password");
                 uid = res.getInt("uid");
             }
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("error at addActivity in ActivityController.java");
-        }
-        if (!password.equals(pass))
-        {
-            System.out.println("attack by changing user in cookie");
-            return "you are dead";
         }
         // 通过cookie验证了
         uid++;  // 用户发帖数加1
@@ -53,20 +47,50 @@ public class ActivityController {
         c.executeUpdate(upd);
         // 已添加入数据库,查询aid
         int aid = -1;
-        res = c.executeQuery("SELECT aid FROM sedb.activity WHERE USERNAME = " + "\"" + username + "\" and uid=" + uid);
+        res = c.executeQuery("SELECT aid FROM sedb.activity WHERE USERNAME = " + "\"" + username +
+                "\" and uid=" + uid + " limit 0, 1");
         try {
             if (res != null && res.first()) {
                 aid = res.getInt("aid");
             }
         } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("error at 62 of addActivity in ActivityController.java");
+            System.out.println("error at 56 of addActivity in ActivityController.java");
         }
         model.addAttribute("aid", aid);
         return "create_a_success";
     }
-    @RequestMapping("query_activity")
-    public void queryActivity(HttpServletRequest request, HttpServletResponse response) {
-    		
+    
+    @RequestMapping("showActivity")
+    public String showActivity(@RequestParam("aid") int aid, ModelMap model, HttpServletResponse response,HttpServletRequest request)
+    {
+        SqlCon c = new SqlCon();
+        String eventName = null, eventTime = null, eventLocation = null, content = null, poster = null, created = null;
+        Timestamp created_ts = null;
+        ResultSet res = c.executeQuery("select * from sedb.activity where aid=" + aid + " limit 0, 1");
+        try {
+            if (res != null && res.first()) {
+                eventName = res.getString("name");
+                eventTime = res.getString("time");
+                eventLocation = res.getString("location");
+                content = res.getString("content");
+                poster = res.getString("username");
+                created_ts = res.getTimestamp("created");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("error at 73 of addActivity in ActivityController.java");
+        }
+        if (created_ts == null) { return "404"; }
+        created = created_ts.toString();
+        String username = (String) request.getSession().getAttribute("username");
+        model.addAttribute("eventName", eventName);
+        model.addAttribute("eventTime", eventTime);
+        model.addAttribute("eventLocation", eventLocation);
+        model.addAttribute("content", content);
+        model.addAttribute("poster", poster);
+        model.addAttribute("created", created);
+        model.addAttribute("username", username);
+        return "show_activity";
     }
 }
